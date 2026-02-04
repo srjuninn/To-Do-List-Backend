cd00f06922e8ac0fc74c0d78524632e8b3c38d66
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2026-02-04 11:17:25 -0300

    feat: add list user, by id, delete

--- src/main/java/com/List/ToDo/service/UserService.java
+++ src/main/java/com/List/ToDo/service/UserService.java
@@ -6,18 +6,48 @@ import com.List.ToDo.dto.UserDTO;
 import com.List.ToDo.entities.UserEntity;
 import com.List.ToDo.repositories.UserRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	public UserService(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-	public UserDTO createUser(UserDTO dto) {
-		UserEntity user = new UserEntity(dto);
-		userRepository.save(user);
-		UserDTO userDto = new UserDTO(user.getName(), user.getEmail());
-		return userDto;
-	}
+    public UserDTO createUser(UserDTO dto) {
+        UserEntity user = new UserEntity(dto);
+        userRepository.save(user);
+        UserDTO userDto = new UserDTO(user.getName(), user.getEmail());
+        return userDto;
+    }
+
+    public List<UserDTO> listUsers() {
+        List<UserEntity> users = userRepository.findAll();
+        List<UserDTO> userDTOs = users.stream().map(user -> new UserDTO(user.getName(), user.getEmail())).toList();
+        return userDTOs;
+    }
+
+    public UserDTO getUserById(Long id) {
+        Optional<UserEntity> userEntityOptional = userRepository.findById(id);
+        if (userEntityOptional.isPresent()) {
+            UserEntity user = userEntityOptional.get();
+            return new UserDTO(user.getName(), user.getEmail());
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteUser(Long id) {
+        Optional<UserEntity> userEntityOptional = userRepository.findById(id);
+        if (userEntityOptional.isPresent()) {
+            userRepository.delete(userEntityOptional.get());
+        } else {
+            throw new RuntimeException("Usuário não encontrado com id: " + id);
+        }
+    }
 }
+
